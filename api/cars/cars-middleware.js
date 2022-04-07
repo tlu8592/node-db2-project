0f17bcbd6f7fe1d942a3627d36e156f06aa568cb
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-07 18:46:22 -0400

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -15,10 +15,31 @@ const checkCarId = async (req, res, next) => {
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const { vin, make, model, mileage } = req.body;
+  if (!vin) {
+    res.status(400).json({
+      message: "vin is missing"
+    })
+  } else if (!make) {
+    res.status(400).json({
+      message: "make is missing"
+    })
+  } else if (!model) {
+    res.status(400).json({
+      message: "model is missing"
+    })
+  } else if (!mileage) {
+    res.status(400).json({
+      message: "mileage is missing"
+    })
+  } else {
+    next();
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
+  
 }
 
 const checkVinNumberUnique = (req, res, next) => {
