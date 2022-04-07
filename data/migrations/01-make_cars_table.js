d0e86b140a87d421cdcc65aee8a4a3af69040ba7
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-07 17:45:42 -0400

    migration complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.string('vin').unique().notNullable();
+    tbl.string('make').notNullable();
+    tbl.string('model').notNullable();
+    tbl.decimal('mileage').notNullable();
+    tbl.string('title');
+    tbl.string('transmission');
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
