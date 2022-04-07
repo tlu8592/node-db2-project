e3a50437a95ff6bd32be2082aa647e054e1f30ce
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-07 18:36:33 -0400

    get endpoint to acquire single car data written

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,9 @@
 // DO YOUR MAGIC
 const router = require('express').Router();
 const Car = require('./cars-model');
+const {
+    checkCarId
+} = require('./cars-middleware');
 
 router.get('/', async (req, res) => {
     const cars = await Car.getAll();
@@ -9,6 +12,20 @@ router.get('/', async (req, res) => {
     } else {
         res.json(cars);
     }
+})
+
+router.get('/:id', checkCarId, (req, res, next) => {
+    res.json(req.car);
+});
+
+// router.post('/', (req, res, next) => {
+
+// })
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+    })
 })
 
 module.exports = router;
