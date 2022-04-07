1ac2b089487dd57891a13543dbbc26c5b446f947
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-07 18:36:02 -0400

    checkCarId middleware written

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,7 +1,16 @@
 const Car = require('./cars-model')
 
-const checkCarId = (req, res, next) => {
+const checkCarId = async (req, res, next) => {
   // DO YOUR MAGIC
+  const car = await Car.getById(req.params.id);
+  if (!car) {
+    res.status(404).json({
+      message: `car with id ${req.params.id} is not found`
+    })
+  } else {
+    req.car = car;
+    next();
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
