783179f85f8973bed2e50201611f9f0e6766ad40
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-14 23:20:40 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
     {
-        vin: "1111111111111",
+        vin: "KM8JUCAG0FU963659",
         make: "toyota",
         model: "prius",
         mileage: 215000,
@@ -9,13 +9,13 @@ const cars = [
         transmission: "manual"
     },
     {
-        vin: "2222222222222",
+        vin: "1G1PE5SB9E7261923",
         make: "toyota",
         model: "corolla",
         mileage: 115000,
         title: "salvage"
     },{
-        vin: "3333333333333",
+        vin: "1FAHP2F84DG296795",
         make: "ford",
         model: "focus",
         mileage: 15000
